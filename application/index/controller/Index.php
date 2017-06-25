ba299831b1b2d4cdcc922a07dbf476bbdef4a7fd
axios <369017150@qq.com>
2017-06-25 21:40:02 +0800

    init

---
+++ application/index/controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | TPR Framework [ Design For Api Develop ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017-2017 http://hanxv.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: axios <axioscros@aliyun.com>
+// +----------------------------------------------------------------------
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class Index extends Controller{
+    public function index(){
+
+    }
+}
