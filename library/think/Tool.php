8dd79371c3a753a532eee4f8e7f8957af0e9a6f5
Axios <axioscros@aliyun.com>
2017-06-28 13:49:11 +0800

    工具类

---
+++ library/think/Tool.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/6/28 13:29
+ */
+namespace think;
+
+class Tool {
+    public static $identity;
+    /**
+     * 获取客户端IP地址
+     * @param int $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
+     * @param bool $adv 是否进行高级模式获取（有可能被伪装）
+     * @return mixed
+     */
+    public static function getClientIp($type = 0, $adv = false) {
+        $type       =  $type ? 1 : 0;
+        static $ip  =   NULL;
+        if ($ip !== NULL) return $ip[$type];
+        if($adv){
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                $pos    =   array_search('unknown',$arr);
+                if(false !== $pos) unset($arr[$pos]);
+                $ip     =   trim($arr[0]);
+            }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+            }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+                $ip     =   $_SERVER['REMOTE_ADDR'];
+            }
+        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        // IP地址合法验证
+        $long = sprintf("%u",ip2long($ip));
+        $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+        return $ip[$type];
+    }
+
+    public static function identity($identity=false){
+        if($identity===false){
+            return self::$identity;
+        }
+        self::$identity = $identity;
+        return $identity;
+    }
+}
