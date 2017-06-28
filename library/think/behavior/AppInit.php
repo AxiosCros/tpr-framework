7e14bc788f01c2527366c72639434478b4ace9e6
Axios <axioscros@aliyun.com>
2017-06-28 13:58:06 +0800

    app_init

---
+++ library/think/behavior/AppInit.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/6/28 13:12
+ */
+namespace think\behavior;
+
+use think\Hook;
+use think\Tool;
+
+class AppInit {
+    public function run(){
+        Tool::identity(1);
+        Hook::add('action_begin' ,'think\\behavior\\ActionBegin');
+        Hook::add('app_end' ,'think\\behavior\\AppEnd');
+        Hook::add('log_write_done', 'think\\behavior\\LogWriteDone');
+        Hook::add('request_end', 'think\\behavior\\RequestEnd');
+    }
+}
