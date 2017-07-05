454df154aad4fc441dea20ae2fc73293bc9724fb
Axios <axioscros@aliyun.com>
2017-07-05 17:23:13 +0800

    Tool增加数组排序功能

--- library/think/Tool.php
+++ library/think/Tool.php
@@ -104,4 +104,48 @@ class Tool {
         $object =  json_decode( json_encode( $object),true);
         return  $object;
     }
+
+    public static function arraySort($array,$sortRule="",$order="asc"){
+        /**
+         * $array = [
+         *              ["book"=>10,"version"=>10],
+         *              ["book"=>19,"version"=>30],
+         *              ["book"=>10,"version"=>30],
+         *              ["book"=>19,"version"=>10],
+         *              ["book"=>10,"version"=>20],
+         *              ["book"=>19,"version"=>20]
+         *      ];
+         */
+        if(is_array($sortRule)){
+            /**
+             * $sortRule = ['book'=>"asc",'version'=>"asc"];
+             */
+            usort($array, function ($a, $b) use ($sortRule) {
+                foreach($sortRule as $sortKey => $order){
+                    if($a[$sortKey] == $b[$sortKey]){continue;}
+                    return (($order == 'desc')?-1:1) * (($a[$sortKey] < $b[$sortKey]) ? -1 : 1);
+                }
+                return 0;
+            });
+        }else if(is_string($sortRule) && !empty($sortRule)){
+            /**
+             * $sortRule = "book";
+             * $order = "asc";
+             */
+            usort($array,function ($a,$b) use ($sortRule,$order){
+                if($a[$sortRule] == $b[$sortRule]){
+                    return 0;
+                }
+                return (($order == 'desc')?-1:1) * (($a[$sortRule] < $b[$sortRule]) ? -1 : 1);
+            });
+        }else{
+            usort($array,function ($a,$b) use ($order){
+                if($a== $b){
+                    return 0;
+                }
+                return (($order == 'desc')?-1:1) * (($a < $b) ? -1 : 1);
+            });
+        }
+        return $array;
+    }
 }
