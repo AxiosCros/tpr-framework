6160c2c508b7c0811a5de52c2f5954be5256b978
axios <axioscros@aliyun.com>
2017-07-14 10:03:49 +0800

    Tool增加curl函数

--- library/think/Tool.php
+++ library/think/Tool.php
@@ -166,4 +166,35 @@ class Tool {
 
         return $str;
     }
+
+    public static function curl($url, $data = [], $post = true, $header = [])
+    {
+        $type = $post ? 1: 0 ;
+
+        curl_init();
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_POST, $type);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+        $output = curl_exec($ch);
+
+        curl_close($ch);
+
+        $result = json_decode($output,true);
+
+        if(empty($result)){
+            return $output;
+        }
+
+        return $result;
+    }
 }
