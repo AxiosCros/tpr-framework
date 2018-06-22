f395e88287771363aa61114ab32149444de456cd
axios <axioscros@aliyun.com>
2018-06-22 09:44:37 +0800

    debug curl get method error

--- library/framework/Tool.php
+++ library/framework/Tool.php
@@ -192,9 +192,10 @@ class Tool {
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        curl_setopt($ch, CURLOPT_POST, $type);
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        if($type){
+            curl_setopt($ch, CURLOPT_POST, $type);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
 
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 
