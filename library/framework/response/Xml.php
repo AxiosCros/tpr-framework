cc0b5e1cfa68006dbe5de8067785bafd7a47d0f5
axios <axioscros@aliyun.com>
2018-06-27 16:24:18 +0800

    跟节点名称默认值为修改data

--- library/framework/response/Xml.php
+++ library/framework/response/Xml.php
@@ -18,7 +18,7 @@ class Xml extends Response
     // 输出参数
     protected $options = [
         // 根节点名
-        'root_node' => 'think',
+        'root_node' => 'data',
         // 根节点属性
         'root_attr' => '',
         //数字索引的子节点名
