d134723bb8c8affe29c666698980702e6368a2f5
axios <axioscros@aliyun.com>
2018-07-19 16:11:11 +0800

    base enter

--- base.php
+++ base.php
@@ -1,65 +1,49 @@
 <?php
+
+namespace tpr\framework;
+
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | TPR-FRAMEWORK2.1 [ BASE ON ThinkPHP5.1 ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2006~2017 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
+// | Author: axioscros <axioscros@aliyun.com>
 // +----------------------------------------------------------------------
 
-define('THINK_VERSION', '5.0.9');
-define('THINK_START_TIME', microtime(true));
-define('THINK_START_MEM', memory_get_usage());
-define('EXT', '.php');
-defined('DS') or define('DS',DIRECTORY_SEPARATOR);
-defined('THINK_PATH') or define('THINK_PATH', __DIR__ . DS);
-define('LIB_PATH', THINK_PATH . 'library' . DS);
-define('CORE_PATH', LIB_PATH . 'framework' . DS);
-define('TRAIT_PATH', LIB_PATH . 'traits' . DS);
-defined('ROOT_PATH') or define('ROOT_PATH', dirname(dirname($_SERVER['SCRIPT_FILENAME'])) . DS);
-defined('APP_PATH') or define('APP_PATH', ROOT_PATH . DS . 'application' . DS);
-defined('EXTEND_PATH') or define('EXTEND_PATH', ROOT_PATH . 'extend' . DS);
-defined('VENDOR_PATH') or define('VENDOR_PATH', ROOT_PATH . 'vendor' . DS);
-defined('RUNTIME_PATH') or define('RUNTIME_PATH', ROOT_PATH . 'runtime' . DS);
-defined('LOG_PATH') or define('LOG_PATH', RUNTIME_PATH . 'log' . DS);
-defined('CACHE_PATH') or define('CACHE_PATH', RUNTIME_PATH . 'cache' . DS);
-defined('TEMP_PATH') or define('TEMP_PATH', RUNTIME_PATH . 'temp' . DS);
-defined('CONF_PATH') or define('CONF_PATH', ROOT_PATH.'config/'); // 配置文件目录
-defined('CONF_EXT') or define('CONF_EXT', EXT); // 配置文件后缀
-defined('ENV_PREFIX') or define('ENV_PREFIX', 'PHP_'); // 环境变量的配置前缀
-defined('LANG_PATH') or define('LANG_PATH',CONF_PATH.'lang'.DIRECTORY_SEPARATOR);
-defined('APP_NAMESPACE') or define('APP_NAMESPACE','app');
-
-// 环境常量
-define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
-define('IS_WIN', strpos(PHP_OS, 'WIN') !== false);
-
 // 载入Loader类
-require CORE_PATH . 'Loader.php';
+require_once __DIR__ . '/library/framework/Loader.php';
 
-// 加载环境变量配置文件
-if (is_file(ROOT_PATH . '.env')) {
-    $env = parse_ini_file(ROOT_PATH . '.env', true);
-    foreach ($env as $key => $val) {
-        $name = ENV_PREFIX . strtoupper($key);
-        if (is_array($val)) {
-            foreach ($val as $k => $v) {
-                $item = $name . '_' . strtoupper($k);
-                putenv("$item=$v");
-            }
-        } else {
-            putenv("$name=$val");
-        }
+require_once __DIR__ . '/helper.php';
+
+define("TPR_FRAMEWORK_NAMESPACE", "\\tpr\\framework\\");
+
+// 注册自动加载
+Loader::register();
+
+// 实现日志接口
+if (interface_exists('Psr\Log\LoggerInterface')) {
+    interface LoggerInterface extends \Psr\Log\LoggerInterface
+    {
+    }
+} else {
+    interface LoggerInterface
+    {
     }
 }
 
-// 注册自动加载
-\tpr\framework\Loader::register();
+$Container = new Container();
 
-// 注册错误和异常处理机制
-\tpr\framework\Error::register();
+// 注册核心类到容器
+$Container['app'] = new App();
 
-// 加载惯例配置文件
-\tpr\framework\Config::set(include THINK_PATH . 'convention' . EXT);
+Loader::addClassAlias([
+    'App'     => \tpr\App::class,
+    'Env'     => \tpr\Env::class,
+    'Config'  => \tpr\Config::class,
+    'Session' => \tpr\Session::class
+]);
+
+// 加载composer AutoFile文件
+Loader::loadComposerAutoloadFiles();
